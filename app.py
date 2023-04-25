4613a9820ffd0fd760992471cfcb74cb9026610b
Your Name <you@example.com>
2023-04-25 02:18:20 -0400

    something

--- app.py
+++ app.py
@@ -9,21 +9,23 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         # Get the user's input from the form
-        track_names = request.form.get('track_names')
+        prompt = request.form.get('prompt')
         playlist_name = request.form.get('playlist_name')
 
-        # Split the user's input into a list of track names
-        track_names = track_names.split(',')
+        # Generate the list of song names based on the prompt
+        track_names = generate_playlist(prompt)
 
-        # Generate the playlist and get the Spotify URL
-        playlist_url = generate_playlist(track_names, playlist_name)
+        # Get the Spotify URIs for the song names
+        track_uris = get_track_uris(track_names)
+
+        # Save the playlist to Spotify and get the Spotify URL
+        playlist_url = save_playlist_to_spotify(playlist_name, track_uris)
 
         # Render the template with the Spotify URL
         return render_template('index.html', playlist_url=playlist_url)
     
     # If the request method is not POST, render the empty form
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 8080)))
