146fd31be5d3b9efc4e5af0cc6f4630370562b16
Your Name <you@example.com>
2023-04-25 02:42:54 -0400

    okokok

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, redirect, url_for
+from generate_playlist import generate_playlist
+from save_to_spotify import save_playlist_to_spotify, get_track_uris
 
 app = Flask(__name__)
 
@@ -12,34 +14,24 @@ SPOTIPY_REDIRECT_URI = os.environ.get("SPOTIPY_REDIRECT_URI")
 auth_manager = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope='playlist-modify-public')
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-def get_track_uris(track_names):
-    track_uris = []
-    for track_name in track_names:
-        results = sp.search(q=track_name, limit=1, type="track")
-        track_uris.append(results['tracks']['items'][0]['uri'])
-    return track_uris
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        playlist_name = request.form['playlist_name']
 
-def save_playlist_to_spotify(playlist_name, track_uris):
-    user_id = sp.me()['id']
-    playlist = sp.user_playlist_create(user_id, playlist_name, public=True, description="")
-    playlist_id = playlist['id']
-    sp.playlist_add_items(playlist_id, track_uris)
-    return playlist['external_urls']['spotify']
+        track_names = generate_playlist(prompt)
+        track_uris = get_track_uris(track_names)
+        playlist_url = save_playlist_to_spotify(playlist_name, track_uris)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+        return redirect(url_for('result', playlist_url=playlist_url))
 
-@app.route('/create_playlist', methods=['POST'])
-def create_playlist():
-    data = request.get_json()
-    playlist_name = data['playlist_name']
-    track_names = data['track_names']
+    return render_template("index.html")
 
-    track_uris = get_track_uris(track_names)
-    playlist_url = save_playlist_to_spotify(playlist_name, track_uris)
-
-    return jsonify({"playlist_url": playlist_url})
+@app.route('/result')
+def result():
+    playlist_url = request.args.get('playlist_url', None)
+    return render_template("result.html", playlist_url=playlist_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
