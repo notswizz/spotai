5139de2811ac04e32b4ed0175df4bdf0ea4288c7
Your Name <you@example.com>
2023-04-25 00:34:14 -0400

    again

--- save_to_spotify.py
+++ save_to_spotify.py
@@ -2,29 +2,23 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# Using the environment variable names you've provided
-CLIENT_ID = os.environ.get('CLIENT_ID')
-CLIENT_SECRET = os.environ.get('SECRET_ID')
-REDIRECT_URI = os.environ.get('SPOTIPY_REDIRECT_URL')
+SPOTIPY_CLIENT_ID = os.environ.get("SPOTIPY_CLIENT_ID")
+SPOTIPY_CLIENT_SECRET = os.environ.get("SPOTIPY_CLIENT_SECRET")
+SPOTIPY_REDIRECT_URI = os.environ.get("SPOTIPY_REDIRECT_URI")
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                               client_secret=CLIENT_SECRET,
-                                               redirect_uri=REDIRECT_URI,
-                                               scope="playlist-modify-public"))
+auth_manager = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope='playlist-modify-public')
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 def get_track_uris(track_names):
     track_uris = []
     for track_name in track_names:
         results = sp.search(q=track_name, limit=1, type="track")
-        track_items = results["tracks"]["items"]
-
-        if track_items:
-            track_uris.append(track_items[0]["uri"])
-
+        track_uris.append(results['tracks']['items'][0]['uri'])
     return track_uris
 
-def save_playlist_to_spotify(user_id, playlist_name, track_uris):
-    new_playlist = sp.user_playlist_create(user_id, playlist_name)
-    sp.user_playlist_add_tracks(user_id, new_playlist['id'], track_uris)
-
-    return new_playlist["external_urls"]["spotify"]
+def create_playlist(playlist_name, track_uris):
+    user_id = sp.me()['id']
+    playlist = sp.user_playlist_create(user_id, playlist_name, public=True, description="")
+    playlist_id = playlist['id']
+    sp.playlist_add_items(playlist_id, track_uris)
+    return playlist['external_urls']['spotify']
