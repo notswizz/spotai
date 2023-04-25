0c70b656e1c6c881fd0d6bed44fc918c364447f6
Your Name <you@example.com>
2023-04-25 15:20:13 -0400

    ok

--- generate_playlist.py
+++ generate_playlist.py
@@ -6,7 +6,7 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 def generate_playlist(prompt):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f"generate 20 songs that follow the prompt: {prompt}. If an artist is mentioned, add that artist and other similar artists tot he playlist. Output: 1. Song1 2. Song2 3. Song3 etc...",
+        prompt=f"generate 20 songs that follow the prompt: {prompt}.",
         temperature=0.7,
         max_tokens=100,
         top_p=1,
