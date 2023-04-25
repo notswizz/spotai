0bc4215e418745825d1a86f90ddea9c83d6d3a48
Your Name <you@example.com>
2023-04-25 00:50:08 -0400

    ok2

--- app.py
+++ app.py
@@ -1,27 +1,44 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+from save_to_spotify import get_track_uris, save_playlist_to_spotify
 from generate_playlist import generate_playlist
-from save_to_spotify import sp, save_playlist_to_spotify, get_track_uris
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        prompt = request.form['prompt']
-        track_names = generate_playlist(prompt)
+        track_names = request.form.getlist('track_names')  # Get list of track names
+        playlist_name = request.form['playlist_name']  # Get playlist name
         track_uris = get_track_uris(track_names)  # Get track URIs
-        user_id = sp.me()["id"]  # Get user id
-        spotify_url = save_playlist_to_spotify(user_id, prompt, track_uris)  # Pass user_id and track_uris
-        return redirect(url_for('result', spotify_url=spotify_url))
+        playlist_url = save_playlist_to_spotify(playlist_name, track_uris)  # Create playlist and add tracks
+        return render_template('results.html', playlist_url=playlist_url)
 
     return render_template('index.html')
 
-@app.route('/result')
-def result():
-    spotify_url = request.args.get('spotify_url', '')
-    return render_template('result.html', spotify_url=spotify_url)
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True, port=int(os.environ.get('PORT', 8080)))
+
+
+
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get the user's input from the form
+        track_names = request.form.get('track_names')
+        playlist_name = request.form.get('playlist_name')
+
+        # Split the user's input into a list of track names
+        track_names = track_names.split(',')
+
+        # Generate the playlist and get the Spotify URL
+        playlist_url = generate_playlist(track_names, playlist_name)
+
+        # Render the template with the Spotify URL
+        return render_template('index.html', playlist_url=playlist_url)
+
+ app = Flask(__name__)
