540719ce820d143085ceefe8dd5e31de1692ad91
Your Name <you@example.com>
2023-04-28 00:51:04 -0400

    gpt3.5turbo

--- generate_playlist.py
+++ generate_playlist.py
@@ -5,7 +5,7 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 def generate_playlist(prompt):
     response = openai.Completion.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo",
         prompt=f"Generate a playlist of 20 songs including the song title and the artist's name, based on the following theme: {prompt}. Please format the output as 'Song Title - Artist Name'.",
         temperature=0.7,
         max_tokens=200,
