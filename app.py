7bcde51a9fc92852d40829784775e88397adebd3
Your Name <you@example.com>
2023-04-28 15:59:02 -0400

    result

--- app.py
+++ app.py
@@ -64,7 +64,8 @@ def index():
 def result():
     playlist_url = request.args.get('playlist_url', None)
     image_url = request.args.get('image_url', None)
-    return render_template("result.html", image_url=image_url, playlist_url=playlist_url, prompt=prompt)
+    return render_template('result.html', playlist_url=playlist_url, image_url=image_url, prompt=user_input)
+
 
 
 if __name__ == '__main__':
